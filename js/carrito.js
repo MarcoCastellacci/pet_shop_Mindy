1c66e81449b1c5d83ed6a367d71b6bb1214efceb
Marco Castellacci <marco.castellacci87@gmail.com>
2022-05-31 15:09:37 -0300

    Funcion Carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,31 +1,86 @@
-// getAPI()
-// async function getAPI() {
-//     await fetch('https://apipetshop.herokuapp.com/api/articulos')
-//         .then(res => res.json())
-//         .then(data => {
-
-//             const productos = data.response;
-
-//             let juguetes = productos.filter(producto => producto.tipo == 'Juguete');
-//             let farmacia = productos.filter(producto => producto.tipo == 'Medicamento');
+let data;
 
 
-//             let arrayCarrito = [];
+async function getAPI() {
+    await fetch('https://apipetshop.herokuapp.com/api/articulos')
+        .then(response => response.json())
+        .then(json => data = json)
+        .catch(error => console.error(error));
 
-//             // escucharEvento(contenedorJuguetes, arrayCarrito);
-//             // escucharEvento(contenedorFarmacia, arrayCarrito);
+    console.log(data);
 
-//         })
-// }
+    let juguetes = data.response.filter(producto => producto.tipo == 'Juguete');
+    let farmacia = data.response.filter(producto => producto.tipo == 'Medicamento');
+    let tablaCarrito = document.getElementById('lista-carrito');
+
+    // let juguetes = [{
+    //         nombre: "Cuerda para morder",
+    //         precio: 350
+    //     },
+    //     {
+    //         nombre: "Frisbee de silicona",
+    //         precio: 320
+    //     }, {
+    //         nombre: "Frisbee de silicona",
+    //         precio: 320
+    //     }, {
+    //         nombre: "Frisbee de silicona",
+    //         precio: 320
+    //     }, {
+    //         nombre: "Frisbee de silicona",
+    //         precio: 320
+    //     }, {
+    //         nombre: "Frisbee de silicona",
+    //         precio: 320
+    //     }, {
+    //         nombre: "Cuerda para morder",
+    //         precio: 350
+    //     }
+    // ]
+
+    function cantidadesParaCarrito(productosCarrito) {
+        let cantidad = {};
+
+        productosCarrito.forEach(producto => {
+            if (!cantidad[producto.nombre]) {
+                cantidad[producto] = {
+                    cantidad: 1,
+                    nombre: producto.nombre,
+                    precio: producto.precio,
+                };
+            } else {
+                cantidad[producto].cantidad = cantidad[producto].cantidad + 1;
+            }
+
+        })
+        return cantidad;
+    }
+    cantidadesParaCarrito(juguetes)
+    console.log(cantidadesParaCarrito(juguetes));
+
+    function mostrarCarrito(carritodecompras) {
+        let html = "";
+        carritodecompras.forEach(producto => {
+            html += `
+                    <tr>
+                        <td>${producto.nombre}</td>
+                        <td>${producto.length}</td>
+                        <td>${producto.precio}</td>
+                        <td>${total}</td>
+                        <td>
+                            <button class="btn btn-danger eliminar" onclick="eliminar(${producto.id})">Eliminar
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="trash bi bi-trash3-fill" viewBox="0 0 16 16">
+                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                            </svg>
+                            </button>
+                        </td>
+                    </tr>
+                `;
+        });
+        tablaCarrito.innerHTML = html;
+    }
+    // mostrarCarrito()
 
 
-
-// console.log(arrayCarrito);
-
-// contenedorJuguetes.addEventListener('click', (e) => {
-//     agregarProducto(e, arrayCarrito)
-// });
-
-// contenedorFarmacia.addEventListener('click', (e) => {
-//     agregarProducto(e, arrayCarrito)
-// });
+}
+getAPI()
