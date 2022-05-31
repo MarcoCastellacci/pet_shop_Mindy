529e58f7e775d38c0260d6dcb9ba8dddce7ee9eb
Marco Castellacci <marco.castellacci87@gmail.com>
2022-05-31 20:21:15 -0300

    Responsive

--- js/carrito.js
+++ js/carrito.js
@@ -54,32 +54,29 @@ async function getAPI() {
     console.log(cantidad);
 
     function mostrarCarrito(carritodecompras) {
+        compra = "";
+        let totalPorProducto = carritodecompras.cantidad * carritodecompras.precio;
         console.log(carritodecompras);
-        let html = "";
-        carritodecompras.forEach(producto => {
-            let totalPorProducto = producto.cantidad * producto.precio;
-            console.log(carritodecompras);
-            html += `
-                    <tr>
-                        <td>${producto.nombre}</td>
-                        <td>${producto.cantidad}</td>
-                        <td>${producto.precio}</td>
+        compra +=
+            `<tr>
+                        <td>${carritodecompras.nombre}</td>
+                        <td>${carritodecompras.cantidad}</td>
+                        <td>${carritodecompras.precio}</td>
                         <td>${totalPorProducto}</td>
                         <td>
-                            <button class="btn btn-danger eliminar" onclick="eliminar(${producto.id})">Eliminar
+                            <button class="btn btn-danger eliminar" onclick="eliminar(${carritodecompras._id})">Eliminar
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="trash bi bi-trash3-fill" viewBox="0 0 16 16">
                             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                             </svg>
                             </button>
                         </td>
-                    </tr>
-                `;
-
-        });
-        tablaCarrito.innerHTML = html;
+                    </tr>`;
+        tablaCarrito.innerHTML = compra;
 
     }
+    console.log(cantidad);
     mostrarCarrito(cantidad)
+
 
     function mostrarTotales(carritodecompras) {
         let total = 0;
