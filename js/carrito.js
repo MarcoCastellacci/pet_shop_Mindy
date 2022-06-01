df9af8c324c6a46a09b2bc3a00462be0d94da2ed
Marco Castellacci <marco.castellacci87@gmail.com>
2022-06-01 00:57:34 -0300

    Ultimos detalles

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,4 @@
 let data;
-
-
 async function getAPI() {
     await fetch('https://apipetshop.herokuapp.com/api/articulos')
         .then(response => response.json())
@@ -8,31 +6,7 @@ async function getAPI() {
         .catch(error => console.error(error));
 
     let tablaCarrito = document.getElementById('lista-carrito');
-
-    let juguetes = [{
-            nombre: "Cuerda para morder",
-            precio: 350
-        },
-        {
-            nombre: "Frisbee de silicona",
-            precio: 320
-        }, {
-            nombre: "Frisbee de silicona",
-            precio: 320
-        }, {
-            nombre: "Frisbee de silicona",
-            precio: 320
-        }, {
-            nombre: "Frisbee de silicona",
-            precio: 320
-        }, {
-            nombre: "Frisbee de silicona",
-            precio: 320
-        }, {
-            nombre: "Cuerda para morder",
-            precio: 350
-        }
-    ]
+    console.log(data);
 
     function cantidadesParaCarrito(productosCarrito) {
         let cantidad = [];
@@ -42,6 +16,7 @@ async function getAPI() {
                     cantidad: 1,
                     nombre: producto.nombre,
                     precio: producto.precio,
+                    id: producto._id
                 };
             } else {
                 cantidad[producto.nombre].cantidad = cantidad[producto.nombre].cantidad + 1;
@@ -50,32 +25,30 @@ async function getAPI() {
         return cantidad;
     }
 
-    let cantidad = cantidadesParaCarrito(juguetes)
+    let cantidad = cantidadesParaCarrito(data.response);
     console.log(cantidad);
 
-    function mostrarCarrito(carritodecompras) {
-        compra = "";
-        let totalPorProducto = carritodecompras.cantidad * carritodecompras.precio;
-        console.log(carritodecompras);
-        compra +=
-            `<tr>
-                        <td>${carritodecompras.nombre}</td>
-                        <td>${carritodecompras.cantidad}</td>
-                        <td>${carritodecompras.precio}</td>
-                        <td>${totalPorProducto}</td>
-                        <td>
-                            <button class="btn btn-danger eliminar" onclick="eliminar(${carritodecompras._id})">Eliminar
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="trash bi bi-trash3-fill" viewBox="0 0 16 16">
-                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-                            </svg>
-                            </button>
-                        </td>
-                    </tr>`;
-        tablaCarrito.innerHTML = compra;
-
+    function mostrarCarrito(carritodecompras, tablaCarrito) {
+        tablaCarrito.innerHTML = "";
+        carritodecompras.forEach(producto => {
+            tablaCarrito.innerHTML +=
+                `<div class="card col-4" style="width: 18rem;">              
+                            <a href="detalles.html?id=${producto._id}">
+                                <img class="img-card"  
+                                    src="${producto.imagen}"> </a>           
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title font-weight-bold">${producto.nombre}</h5>
+                                <p class="card-text contenedor-parrafo" style="margin-top: auto;">${producto.descripcion}</p>        
+                                <div class="text-center" style="margin-top: auto;">
+                                    <p class="card-text mb-1 fs-5 precio">$${producto.precio}</p>
+                                    <a href="#" class="btn btn-naranja mt-3" onclick="agregarProducto('${producto._id}')">Agregar al carrito</a>
+                                </div>
+                            </div>
+                        </div>`;
+        })
     }
-    console.log(cantidad);
-    mostrarCarrito(cantidad)
+    console.log(cantidad, tablaCarrito);
+    mostrarCarrito(cantidad, tablaCarrito)
 
 
     function mostrarTotales(carritodecompras) {
@@ -84,10 +57,9 @@ async function getAPI() {
         carritodecompras.forEach(producto => {
             let totalPorProducto = producto.cantidad * producto.precio;
             total += totalPorProducto;
-            html += `
-                <p>${totalPorProducto}</p>
-            `;
+            html += ` <p> ${total}</p>`;
         })
+
         document.getElementById('total').innerHTML = html;
     }
     mostrarTotales(cantidad)
